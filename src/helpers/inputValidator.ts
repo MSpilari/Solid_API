df3a9c57cc1132a9851c983c9f88f9a23d1bd918
MSpilari <mb.spilari@hotmail.com>
2024-04-08 16:37:17 -0300

    feat: Creating inputValidator

---
+++ src/helpers/inputValidator.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express'
+import Joi from 'joi'
+
+class InputValidator {
+	private schema: Joi.ObjectSchema
+
+	constructor(schema: Joi.ObjectSchema<any>) {
+		this.schema = schema
+	}
+
+	restaurantValidator = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		const userInput = req.body
+
+		try {
+			await this.schema.validateAsync(userInput)
+
+			next()
+		} catch (error) {
+			next(error)
+		}
+	}
+}
+
+export { InputValidator }
