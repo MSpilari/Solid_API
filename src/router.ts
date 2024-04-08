43f05ace2dbbdb6be29f60a3874ed00e52e7c056
MSpilari <mb.spilari@hotmail.com>
2024-04-08 17:03:20 -0300

    feat: Add newrestaurant route

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,18 @@ import express from 'express'
 import { RestaurantController } from './controllers/Restaurant.controller'
 import { RestaurantService } from './services/Restaurant.service'
 import { restaurantRepository } from './repositories/restaurantRepo'
+import { InputValidator } from './helpers/inputValidator'
+import { restaurantSchema } from './joiSchemas/restaurantSchema.joi'
 
 const newRestaurantService = new RestaurantService(restaurantRepository)
-const { getAllRestaurants } = new RestaurantController(newRestaurantService)
+const { getAllRestaurants, setNewRestaurant } = new RestaurantController(
+	newRestaurantService
+)
+const { restaurantValidator } = new InputValidator(restaurantSchema)
 
 const router = express.Router()
 
 router.get('/allrestaurants', getAllRestaurants)
+router.post('/newrestaurant', restaurantValidator, setNewRestaurant)
 
 export { router }
