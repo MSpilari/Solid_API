6dd52b2a0ab3b42750f07ece9d64dad25251e454
MSpilari <mb.spilari@hotmail.com>
2024-04-09 16:41:40 -0300

    feat: Add create a new product route

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,9 @@ import { RestaurantService } from './services/Restaurant.service'
 import { restaurantRepository } from './repositories/restaurantRepo'
 import { InputValidator } from './helpers/inputValidator'
 import { restaurantSchema } from './joiSchemas/restaurantSchema.joi'
+import { ProductService } from './services/Product.service'
+import { productRepository } from './repositories/productRepo'
+import { ProductController } from './controllers/Product.controller'
 
 const { restaurantValidator } = new InputValidator(restaurantSchema)
 
@@ -16,6 +19,9 @@ const {
 	deleteRestaurant
 } = new RestaurantController(newRestaurantService)
 
+const newProductService = new ProductService(productRepository)
+const { setNewProduct } = new ProductController(newProductService)
+
 const router = express.Router()
 
 router.get('/allrestaurants', getAllRestaurants)
@@ -23,5 +29,7 @@ router.post('/newrestaurant', restaurantValidator, setNewRestaurant)
 router.get('/restaurant/:id', getRestaurant)
 router.put('/restaurant/:id', restaurantValidator, updateRestaurant)
 router.delete('/restaurant/:id', deleteRestaurant)
+
+router.post('/newproduct', setNewProduct)
 
 export { router }
