2c125998586b70509505f712e067de8116685c43
MSpilari <mb.spilari@hotmail.com>
2024-04-10 11:18:32 -0300

    feat: Add getProductsFromRestaurant method

--- src/controllers/Product.controller.ts
+++ src/controllers/Product.controller.ts
@@ -16,6 +16,21 @@ class ProductController {
 			next(error)
 		}
 	}
+
+	getProductsFromRestaurant = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		try {
+			const { id } = req.params
+			const productFromRestaurant =
+				await this.productService.productsFromRestaurant(id)
+			return res.status(200).json(productFromRestaurant)
+		} catch (error) {
+			next(error)
+		}
+	}
 }
 
 export { ProductController }
