407081c43c44df882ea677249b040e002bd3b9b0
MSpilari <mb.spilari@hotmail.com>
2024-04-10 11:29:51 -0300

    refactor: inputValidator change from a class to function

--- src/helpers/inputValidator.ts
+++ src/helpers/inputValidator.ts
@@ -1,18 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
 import Joi from 'joi'
 
-class InputValidator {
-	private schema: Joi.ObjectSchema
-
-	constructor(schema: Joi.ObjectSchema<any>) {
-		this.schema = schema
-	}
-
-	inputValidator = async (req: Request, res: Response, next: NextFunction) => {
+const inputValidator = (schema: Joi.ObjectSchema) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
 		const userInput = req.body
 
 		try {
-			await this.schema.validateAsync(userInput)
+			await schema.validateAsync(userInput)
 
 			next()
 		} catch (error) {
@@ -21,4 +15,4 @@ class InputValidator {
 	}
 }
 
-export { InputValidator }
+export { inputValidator }
