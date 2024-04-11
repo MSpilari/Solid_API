6458238285cab7424beb19b8a04e902174818887
MSpilari <mb.spilari@hotmail.com>
2024-04-11 16:29:00 -0300

    feat: errorHandler can return error status dinamically

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,4 +1,6 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
+import { CustomErrorHandler } from './customErrorHandler'
+import Joi from 'joi'
 
 const errorHandler: ErrorRequestHandler = (
 	error: Error,
@@ -6,6 +8,14 @@ const errorHandler: ErrorRequestHandler = (
 	res: Response,
 	next: NextFunction
 ) => {
+	if (error instanceof CustomErrorHandler) {
+		return res.status(error.status).json({ message: error.message })
+	}
+
+	if (error instanceof Joi.ValidationError) {
+		return res.status(400).json({ message: error.message })
+	}
+
 	return res.status(500).json({ message: error.message })
 }
 
